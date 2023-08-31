977002168d4578e4587667ee6d0498cfeeabce6b
Mczella <markaczova@gmail.com>
2023-08-31 21:35:46 +0200

    Add contactRef

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -4,9 +4,9 @@ import {
     Heading,
 } from "@chakra-ui/react";
 
-const Skills = ({isMobile, viewportHeight}) => {
+const Skills = ({isMobile, viewportHeight, contactRef}) => {
     return (
-        <Flex style={!isMobile && viewportHeight>=575?{ scrollSnapAlign: 'start', height: '100vh'}: null} position={'relative'} flexDir={'column'} >
+        <Flex ref={contactRef} style={!isMobile && viewportHeight>=665?{ scrollSnapAlign: 'start', height: '100vh'}: null} position={'relative'} flexDir={'column'} >
             <Flex maxW="75%" py={'30px'} m={'auto'}>
                 <Heading  size="xl">
                     Contact and more informations
