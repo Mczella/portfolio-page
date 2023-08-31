0e6d8e3d14ff57c7fdead52ec160b1931b04fd03
Mczella <markaczova@gmail.com>
2023-08-31 21:36:14 +0200

    Add automatic scroll buttons

--- src/Header.jsx
+++ src/Header.jsx
@@ -13,9 +13,10 @@ import React, {useEffect, useRef, useState} from "react"
 import {ReactSketchCanvas} from "react-sketch-canvas"
 import Scroll from "./Scroll"
 import Skills from "./Skills"
-import {motion, useAnimate} from "framer-motion"
+import {motion, useAnimate, useInView} from "framer-motion"
 import Name from "./Name"
 import Contact from "./Contact"
+import useScroll from "./UseScroll";
 
 
 const Header = () => {
@@ -28,6 +29,13 @@ const Header = () => {
     const [isMobile, setIsMobile] = useState(false)
     const [isHuge, setIsHuge] = useState(false)
     const [viewportHeight, setViewportHeight] = useState(null)
+    const [skillsRef, scrollToComponent1] = useScroll()
+    const [experienceRef, scrollToComponent2] = useScroll()
+    const [contactRef, scrollToComponent3] = useScroll()
+    const [topRef, scrollToComponent4] = useScroll()
+    const { ref, inView } = useInView({
+        triggerOnce: true, // Only trigger once when the element comes into view
+    })
 
     useEffect(() => {
         setViewportHeight(visualViewport.height)
@@ -141,15 +149,19 @@ const Header = () => {
     return (
         <Box flexDir={'column'}
              style={
-                !isMobile && viewportHeight >= 575 ?
+                !isMobile && viewportHeight >= 665 ?
                     {scrollSnapType: 'y mandatory', overflowY: 'scroll'}
                     :
                     null
             }
              height={'100vh'}>
             <Flex flexDir="column" justify="center" alignItems="center" spacing="20" w="75%" m="auto">
+                <motion.div initial={{ height: 0 }}
+                            animate={{ height: inView ? '100vh' : 0 }}
+                            transition={{ duration: 0.5 }}>
                 <Box bg={"#ce016d"} w='10px' h={boxHeight} position="fixed" zIndex={-1}>
                 </Box>
+                </motion.div>
             </Flex>
 
             {isDrawing ? (
@@ -192,12 +204,38 @@ const Header = () => {
                 </Button>
             )}
 
+            {viewportHeight >= 665?
+                <ButtonGroup size={'xs'}
+                             zIndex={2}
+                             position="fixed"
+                             variant='outline'
+                             isAttached
+                             textColor={'pink.400'}
+                             bottom="40px"
+                             right="10px">
+                <Button onClick={scrollToComponent4}>
+                    Top
+                </Button>
+                <Button onClick={scrollToComponent2}>
+                    Experience
+                </Button>
+                <Button onClick={scrollToComponent1}>
+                    Skills
+                </Button>
+                <Button onClick={scrollToComponent3}>
+                    Contact
+                </Button>
+                </ButtonGroup>
+            :
+            null}
+
             <Flex bg={'#ce016d'}
                   direction={['column', 'column', 'column', 'row']}
                   justify={'space-between'}
                   h={'100vh'}
                   style={{scrollSnapAlign: 'start'}}
-                  position={'relative'}>
+                  position={'relative'}
+                  ref={topRef} >
 
                 <Stack
                     width={'100%'}
@@ -221,33 +259,38 @@ const Header = () => {
                 <Box py={['10px', '25px', '50px']} pr={['10px', '25px', '50px']}>
                     <Image zIndex={0}
                            objectFit='contain'
-                           minW={'250px'}
+                           minW={'200px'}
                            height={'400px'}
+                           maxH={isMobile? '50vh':null}
+                           maxW={'80%'}
                            left={0}
                            src='me.png'
                            alt='Me'/>
                 </Box>
-                <Spacer/>
+
                 <Flex direction={'column'} alignItems="center" justifyContent="center">
-                    <Box pr={['0', '50px', '100px']}
-                         pt={['20px', '50px', '100px']}
-                         width={['80%', '80%', '800px']}>
+                    <Box pr={['0', '50px','50px', '100px']}
+                         pt={['20px', '50px', '50px', '100px']}
+                         width={['80%', '80%', '80%', '800px']}>
                         <Name/>
                     </Box>
-                    <Box w={['80%', '80%', '800px']}
-                         pr={['10px', '50px', '100px']}
-                         py={['20px', '30px', '50px']}>
+                    <Box w={['80%', '80%', '80%', '800px']}
+                         pr={['10px', '50px', '50px', '100px']}
+                         py={['20px', '30px', '30px', '50px']}>
                         <motion.div variants={about} initial="hidden" animate="visible">
-                            <Text color="white" fontSize={['3xl', '4xl', '5xl']} textAlign="left">
+                            <Text color="white" fontSize={['2xl', '3xl', '3xl', '5xl']} textAlign="left">
                                 SOFTWARE DEVELOPER
                             </Text>
                         </motion.div>
                     </Box>
                 </Flex>
             </Flex>
-            <Scroll isMobile={isMobile} viewportHeight={viewportHeight}/>
-            <Skills isMobile={isMobile} viewportHeight={viewportHeight}/>
-            <Contact isMobile={isMobile} viewportHeight={viewportHeight}/>
+            <Scroll isMobile={isMobile} viewportHeight={viewportHeight} experienceRef={experienceRef} ref={ref}/>
+
+
+            <Skills isMobile={isMobile} viewportHeight={viewportHeight} skillsRef={skillsRef}/>
+
+            <Contact isMobile={isMobile} viewportHeight={viewportHeight} contactRef={contactRef}/>
         </Box>)
 }
 
