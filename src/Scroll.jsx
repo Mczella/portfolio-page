9693c621793301877adcfa27593a228e3975e72d
Mczella <markaczova@gmail.com>
2023-09-02 22:39:38 +0200

    Edit text sizes and responsivity

--- src/Scroll.jsx
+++ src/Scroll.jsx
@@ -22,15 +22,9 @@ const Scroll = ({isMobile, viewportHeight, experienceRef, newColor, colorScheme}
     return (
         <><Flex ref={experienceRef} position={'relative'} style={!isMobile && viewportHeight>=665?{ scrollSnapAlign: 'start', height: '100vh'}: null} flexDir="column" justify="center" alignItems="center" spacing="20" w="75%" m="auto" py={'30px'} zIndex={1}>
                 <Grid position={'relative'} templateColumns={columnLayout} templateRows='repeat(5, 1fr)' gap={2}>
-                    <GridItem bg="white"
-                              colSpan={isMobile ? 1 : 3}
-                              style={{minWidth: '200px'}}
-                              display="flex"
-                              justifyContent="center"
-                              alignItems="center">
-                        <Box flex="1" textAlign={isMobile ? "center" : "right"}>
-                            <Heading size={'md'}>Pyladies Python Course</Heading>
-                        </Box>
+                    <GridItem
+                        colSpan={isMobile ? 1 : 3}
+                        style={{minWidth: '200px'}}>
                     </GridItem>
                     <GridItem colSpan={1} rowSpan={5} display="flex" justifyContent="center" alignItems="center">
                         <Circle
@@ -44,17 +38,17 @@ const Scroll = ({isMobile, viewportHeight, experienceRef, newColor, colorScheme}
                             </Text>
                         </Circle>
                     </GridItem>
-                    <GridItem bg="white"
+                    <GridItem
                               colSpan={isMobile ? 1 : 3}
                               style={{minWidth: '200px'}}>
                     </GridItem>
-                    <GridItem bg="white"
+                    <GridItem
                               colSpan={isMobile ? 1 : 3}
                               rowSpan={4}
                               style={{minWidth: '200px'}}>
                         <Box textAlign={isMobile ? "justify" : "right"}>
-                            <Heading size={'sm'}>Flappy Bird</Heading>
-                            <Text>A Python adaptation of Flappy Bird written as a means to practice and apply the skills
+                            <Heading py={'10px'} bg="white" size={'md'}>Pyladies Python Course - Flappy Bird</Heading>
+                            <Text bg="white">A Python adaptation of Flappy Bird written as a means to practice and apply the skills
                                 I acquired through the Pyladies course.</Text>
 
                             <Badge position={'relative'} zIndex={2} colorScheme={colorScheme} px={2} rounded={'full'}>
@@ -64,13 +58,13 @@ const Scroll = ({isMobile, viewportHeight, experienceRef, newColor, colorScheme}
                             </Badge>
                         </Box>
                     </GridItem>
-                    <GridItem bg="white"
+                    <GridItem
                               colSpan={isMobile ? 1 : 3}
                               rowSpan={4}
                               style={{minWidth: '200px'}}>
                         <Box textAlign={isMobile ? "justify" : "left"}>
-                            <Heading size={'sm'}>Rohlik.cz Shopping Automation</Heading>
-                            <Text>A Python project aimed at managing groceries and automating shopping.</Text>
+                            <Heading py={'10px'} textAlign={'left'} bg="white" size={'md'}>Rohlik.cz Shopping Automation Project</Heading>
+                            <Text bg="white">A Python project aimed at managing groceries and automating shopping.</Text>
 
                             <Badge position={'relative'} zIndex={2} colorScheme={colorScheme} px={2} rounded={'full'}>
                                 <Link href={'https://github.com/Mczella/Rohlik.cz-automation'} color={newColor} isExternal>
@@ -83,15 +77,9 @@ const Scroll = ({isMobile, viewportHeight, experienceRef, newColor, colorScheme}
 
                 <Grid mt={'40px'} position={'relative'} templateColumns={columnLayout}
                       templateRows={isMobile ? 'repeat(5, 1fr)' : 'repeat(7, 1fr)'} gap={2}>
-                    <GridItem bg="white"
-                              colSpan={isMobile ? 1 : 3}
-                              style={{minWidth: '200px'}}
-                              display="flex"
-                              justifyContent="center"
-                              alignItems="center">
-                        <Box flex="1" textAlign={isMobile ? "center" : "right"}>
-                            <Heading size={'md'}>ReactGirls Academy</Heading>
-                        </Box>
+                    <GridItem
+                        colSpan={isMobile ? 1 : 3}
+                        style={{minWidth: '200px'}}>
                     </GridItem>
                     <GridItem colSpan={1} rowSpan={5} display="flex" justifyContent="center" alignItems='flex-end'>
                         <Circle
@@ -105,17 +93,17 @@ const Scroll = ({isMobile, viewportHeight, experienceRef, newColor, colorScheme}
                             </Text>
                         </Circle>
                     </GridItem>
-                    <GridItem bg="white"
+                    <GridItem
                               colSpan={isMobile ? 1 : 3}
                               style={{minWidth: '200px'}}>
                     </GridItem>
-                    <GridItem bg="white"
+                    <GridItem
                               colSpan={isMobile ? 1 : 3}
                               rowSpan={3}
                               style={{minWidth: '200px'}}>
                         <Box textAlign={isMobile ? "justify" : "right"}>
-                            <Heading size={'sm'}>Bomberman</Heading>
-                            <Text>A JavaScript project featuring a fully functional classic game developed during my
+                            <Heading py={'10px'} textAlign={isMobile? 'left' : 'right'} bg="white" size={'md'}>ReactGirls Academy - Bomberman</Heading>
+                            <Text bg="white">A JavaScript project featuring a fully functional classic game developed during my
                                 time at the ReactGirls Academy and continued afterwards. Check out the code on my GitHub page.</Text>
                         <Badge position={'relative'} zIndex={2} colorScheme={colorScheme} px={2} rounded={'full'}>
                             <Link href={'https://javascript-bomberman.netlify.app/'} color={newColor} isExternal>
@@ -124,22 +112,13 @@ const Scroll = ({isMobile, viewportHeight, experienceRef, newColor, colorScheme}
                         </Badge>
                         </Box>
                     </GridItem>
-                    <GridItem bg="white"
+                    <GridItem
                               colSpan={isMobile ? 1 : 3}
                               rowSpan={3}
                               style={{minWidth: '200px'}}>
                         <Box textAlign={isMobile ? "justify" : "left"}>
-                            <Heading size={'sm'}>Google Cybersecurity Professional Certificate</Heading>
-                            <Text>In progress, to be finished by December.</Text>
-                        </Box>
-                    </GridItem>
-                    <GridItem bg="white"
-                              colSpan={isMobile ? 1 : 3}
-                              rowSpan={3}
-                              style={{minWidth: '200px'}}>
-                        <Box textAlign={isMobile ? "justify" : "right"}>
-                            <Heading size={'sm'}>Wishlist React App</Heading>
-                            <Text>Wishlist is a React project built with Firebase and Chakra UI, designed to
+                            <Heading py={'10px'} bg="white" size={'md'}>Wishlist React App Project</Heading>
+                            <Text bg="white">Wishlist is a React project built with Firebase and Chakra UI, designed to
                                 simplify
                                 the process of creating gift registries. It provides users with the ability to
                                 create,
@@ -152,13 +131,22 @@ const Scroll = ({isMobile, viewportHeight, experienceRef, newColor, colorScheme}
                             </Badge>
                         </Box>
                     </GridItem>
-                    <GridItem bg="white"
+                    <GridItem
+                              colSpan={isMobile ? 1 : 3}
+                              rowSpan={3}
+                              style={{minWidth: '200px'}}>
+                        <Box textAlign={isMobile ? "justify" : "right"}>
+                            <Heading py={'10px'} textAlign={isMobile ? "left" : "right"} bg="white" size={'md'}>Google Cybersecurity Professional Certificate</Heading>
+                            <Text bg="white">Python | Linux | SQL | Security Information and Event Management (SIEM) tools. In progress, to be finished by December. Learning about common risks, threats and vulnerabilities, as well as the techniques to mitigate them.</Text>
+                        </Box>
+                    </GridItem>
+                    <GridItem
                               colSpan={isMobile ? 1 : 3}
                               rowSpan={3}
                               style={{minWidth: '200px'}}>
                         <Box textAlign={isMobile ? "justify" : "left"}>
-                            <Heading size={'sm'}>Pyladies Data Course</Heading>
-                            <Text>Starting in September.</Text>
+                            <Heading py={'10px'} bg="white" size={'md'}>Pyladies Data Course</Heading>
+                            <Text bg="white">Data Science | Jupyter notebook | Pandas | Machine learning. Starting in September.</Text>
                         </Box>
                     </GridItem>
                 </Grid>
