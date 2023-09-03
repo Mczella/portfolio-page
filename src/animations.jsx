407ae5deff2a5a788de4dd2834546efa1ce31d00
Mczella <markaczova@gmail.com>
2023-09-03 21:56:51 +0200

    Animation for header

---
+++ src/animations.jsx
@@ -0,0 +1,13 @@
+export const about = {
+    hidden: {
+        opacity: 0,
+        y: 300
+    }, visible: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            bounce: 0.4, duration: 1.5, ease: "easeInOut", delay: 2.5
+        }
+    }
+}
