681300bb089ada41213af7cebc356831b97c1861
Mczella <markaczova@gmail.com>
2023-09-03 20:27:29 +0200

    Add device sizes to context

--- src/ViewportContext.jsx
+++ src/ViewportContext.jsx
@@ -1,11 +1,22 @@
 import {createContext, useEffect, useState} from "react";
+import {useBreakpointValue} from "@chakra-ui/react";
 
 export const ViewportContext = createContext()
 
 export const ViewportProvider = ({children}) => {
     const [viewportHeight, setViewportHeight] = useState(null)
     const [viewportWidth, setViewportWidth] = useState(null)
+    const [isMobile, setIsMobile] = useState(false)
+    const [isHuge, setIsHuge] = useState(false)
 
+    const deviceSize = useBreakpointValue({
+        base: 'smallest',
+        sm: 'small',
+        md: 'medium',
+        lg: "large",
+        xl: 'extra large',
+        '2xl': 'huge'
+    })
     useEffect(() => {
         setViewportHeight(visualViewport.height)
         setViewportWidth(visualViewport.width)
@@ -15,8 +26,17 @@ export const ViewportProvider = ({children}) => {
         setViewportHeight(visualViewport.height)
         setViewportWidth(visualViewport.width)
     }
+
+    useEffect(() => {
+        setIsMobile(
+            deviceSize === "medium" ||
+            deviceSize === "smallest" ||
+            deviceSize === "small"
+        )
+        setIsHuge(deviceSize === "huge")
+    }, [deviceSize])
     return (
-        <ViewportContext.Provider value={{viewportWidth, viewportHeight}}>
+        <ViewportContext.Provider value={{viewportWidth, viewportHeight, isMobile, isHuge}}>
             {children}
         </ViewportContext.Provider>
     )
